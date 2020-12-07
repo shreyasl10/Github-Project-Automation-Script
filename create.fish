fb012e3030b68133e855efd96bec18b78633a52b
shreyasl10 <shreyaslakshminarayanan10@gmail.com>
2020-12-07 20:31:08 +0530

    Changed script to curl

--- create.fish
+++ create.fish
@@ -1,9 +1,12 @@
-function create        
-	cd Documents/Projects
-	mkdir $argv
-	cd $argv
-	git init
-	gh repo create
-	git push -u origin master
-	code .
+ function create        
+		cd Documents/Projects
+		mkdir $argv
+	    cd $argv
+	    git init
+	    set pat (gpg -d ~/Documents/PAT.txt.gpg)
+		set rep '"'name'"':'"'$argv'"'
+		echo $rep
+		curl -u "shreyasl10:$pat" https://api.github.com/user/repos -d "{$rep}"
+		git push -u origin master
+		code .
 end
