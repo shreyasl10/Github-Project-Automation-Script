4f6098ee215dddacbd69b9debb46fd7542a0d523
Shreyas L <shreyaslakshminarayanan10@gmail.com>
2020-12-28 20:18:35 +0530

    Update create.fish

--- create.fish
+++ create.fish
@@ -1,11 +1,10 @@
  function create        
 		cd Documents/Projects
 		mkdir $argv
-	    cd $argv
-	    git init
-	    set pat (gpg -d ~/Documents/PAT.txt.gpg)
+	        cd $argv
+	   	git init
+	    	set pat (gpg -d ~/Documents/PAT.txt.gpg)
 		set rep '"'name'"':'"'$argv'"'
-		echo $rep
 		curl -u "shreyasl10:$pat" https://api.github.com/user/repos -d "{$rep}"
 		git push -u origin master
 		code .
